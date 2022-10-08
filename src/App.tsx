fb034e56d49336dcd313b7e1d7df077efec3e4f9
Parssa Kyanzadeh <33424333+parssak@users.noreply.github.com>
2022-10-08 10:36:58 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className='container mx-auto py-6'>
+      <h1 className="font-bold text-3xl">Starter Template</h1>
+      <p>Vite, React, TypeScript, TailwindCSS 3</p>
+    </div>
+  );
+}
+
+export default App;
