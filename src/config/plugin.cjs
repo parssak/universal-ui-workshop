b1ff86a6ab84f6d2876fe3359611e59ff04a2651
parssak <parssak@gmail.com>
2022-11-06 12:05:53 -0500

    feat: added customize option to plugin

--- src/config/plugin.cjs
+++ src/config/plugin.cjs
@@ -1,145 +1,150 @@
 const plugin = require("tailwindcss/plugin");
 const colors = require("tailwindcss/colors");
-const Color = require("color");
 
-// -----------------------------------------------------------------
-// Extract RGB channels from a color
-// -----------------------------------------------------------------
-
-function getRgbChannels(color) {
-  return Color(color).rgb().array().join(" ");
+function getRgbChannels(hex) {
+  const padded = hex.length === 4 ? "#" + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3] : hex;
+  return padded
+    .match(/\w\w/g)
+    .map((x) => parseInt(x, 16))
+    .join(" ");
 }
-
-// -----------------------------------------------------------------
-// Themes definition
-// -----------------------------------------------------------------
 
 const getColorsForTheme = (color, isDark = false) => {
   const STEPS = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900"];
 
   const C = colors[color];
-  const cMap = (l, d) => C[STEPS[isDark ? d : l]];
+  const colorMap = (light, dark) => C[STEPS[isDark ? dark : light]];
 
   return {
-    "text-base": cMap(8, 0),
-    "text-inverted": cMap(0, 8),
-    "text-muted": cMap(5, 3),
-    "text-active": cMap(9, 1),
+    "text-base": colorMap(8, 0),
+    "text-inverted": colorMap(0, 8),
+    "text-muted": colorMap(5, 3),
+    "text-active": colorMap(9, 1),
     "bg-pure": isDark ? colors.black : colors.white,
-    "bg-base": cMap(0, 8),
-    "bg-inverted": cMap(8, 0),
-    "bg-active": cMap(2, 6),
-    "border-base": cMap(4, 5),
-    "border-inverted": cMap(5, 4),
-    "border-active": cMap(3, 5)
+    "bg-base": colorMap(0, 8),
+    "bg-inverted": colorMap(8, 0),
+    "bg-active": colorMap(2, 6),
+    "border-base": colorMap(4, 5),
+    "border-inverted": colorMap(5, 4),
+    "border-active": colorMap(3, 5)
   };
 };
 
-const THEMES_CONFIG = [
-  {
-    name: "neutral",
-    color: "gray"
-  },
-  {
-    name: "brand",
-    color: "indigo"
-  },
-  {
-    name: "error",
-    color: "rose"
-  },
-  {
-    name: "success",
-    color: "emerald"
-  },
-  {
-    name: "warning",
-    color: "orange"
-  },
-  {
-    name: "info",
-    color: "sky"
-  }
-];
-
-const SIZES_CONFIG = [
-  {
-    name: "xs",
-    sizes: {
-      px: "0.25rem",
-      py: "0.125rem",
-      text: "0.75rem",
-      line: "1rem"
-    }
-  },
-  {
-    name: "sm",
-    sizes: {
-      px: "0.5rem",
-      py: "0.25rem",
-      text: "0.875rem",
-      line: "1.25rem"
-    }
-  },
-  {
-    name: "md",
-    sizes: {
-      px: "0.75rem",
-      py: "0.375rem",
-      text: "1rem",
-      line: "1.5rem"
-    }
-  },
-  {
-    name: "lg",
-    sizes: {
-      px: "1rem",
-      py: "0.375rem",
-      text: "1.125rem",
-      line: "1.5rem"
-    }
-  },
-  {
-    name: "xl",
-    sizes: {
-      px: "1.25rem",
-      py: "0.5rem",
-      text: "1.25rem",
-      line: "1.5rem"
-    }
-  }
-];
-
-// Create light/dark themes for each color
-const themes = THEMES_CONFIG.reduce((acc, theme) => {
-  const { name, color } = theme;
-  return [
-    ...acc,
-    {
-      name,
-      colors: getColorsForTheme(color)
-    },
-    {
-      name: `${name}-dark`,
-      colors: getColorsForTheme(color, true)
-    }
-  ];
-}, []);
-
-const sizes = SIZES_CONFIG;
-
-// -----------------------------------------------------------------
-// Tailwind CSS plugin
-// -----------------------------------------------------------------
-
 module.exports = plugin(
-  function ({ addBase, addVariant }) {
-    // -----------------------------------------------------------------
-    // Root scope CSS variables
-    // -----------------------------------------------------------------
+  function ({ addBase, addVariant, config }) {
+    const DEFAULT_THEMES_CONFIG = [
+      {
+        name: "neutral",
+        color: "neutral"
+      },
+      {
+        name: "brand",
+        color: "indigo"
+      },
+      {
+        name: "error",
+        color: "rose"
+      },
+      {
+        name: "success",
+        color: "emerald"
+      },
+      {
+        name: "warning",
+        color: "orange"
+      },
+      {
+        name: "info",
+        color: "sky"
+      }
+    ];
 
-    const defaultColors = themes[0].colors;
-    const defaultSizes = sizes[2].sizes;
+    // filter out all config defined themes
+    const customThemes = config("theme.universalUI.themes") || [];
+    const themeConfig = [
+      ...DEFAULT_THEMES_CONFIG.filter((theme) => !customThemes.find((t) => t.name === theme.name)),
+      ...customThemes
+    ];
+
+    // Create light/dark themes for each color
+    const themes = themeConfig.reduce((acc, theme) => {
+      const { name, color } = theme;
+      return [
+        ...acc,
+        {
+          name,
+          colors: getColorsForTheme(color, false)
+        },
+        {
+          name: `${name}-dark`,
+          colors: getColorsForTheme(color, true)
+        }
+      ];
+    }, []);
+
+    const DEFAULT_SIZES_CONFIG = [
+      {
+        name: "xs",
+        sizes: {
+          px: "0.25rem",
+          py: "0.125rem",
+          text: "0.75rem",
+          line: "1rem"
+        }
+      },
+      {
+        name: "sm",
+        sizes: {
+          px: "0.5rem",
+          py: "0.25rem",
+          text: "0.875rem",
+          line: "1.25rem"
+        }
+      },
+      {
+        name: "md",
+        sizes: {
+          px: "0.75rem",
+          py: "0.375rem",
+          text: "1rem",
+          line: "1.5rem"
+        }
+      },
+      {
+        name: "lg",
+        sizes: {
+          px: "1rem",
+          py: "0.375rem",
+          text: "1.125rem",
+          line: "1.5rem"
+        }
+      },
+      {
+        name: "xl",
+        sizes: {
+          px: "1.25rem",
+          py: "0.5rem",
+          text: "1.25rem",
+          line: "1.5rem"
+        }
+      }
+    ];
+
+    const customSizes = config("theme.universalUI.sizes") || [];
+    const sizes = [
+      ...DEFAULT_SIZES_CONFIG.filter((size) => !customSizes.find((s) => s.name === size.name)),
+      ...customSizes
+    ];
+
+    const defaultColors = themes.find((t) => t.name === "neutral").colors;
+    if (!defaultColors) {
+      throw new Error("Default theme not found");
+    }
+
+    const defaultSizes = sizes.find((s) => s.name === "md").sizes;
+    if (!defaultSizes) {
+      throw new Error("Default sizes not found");
+    }
 
     const getCSSColorVariables = (colors) => {
       return Object.fromEntries(
@@ -163,10 +168,8 @@ module.exports = plugin(
         ...getCSSSizeVariables(defaultSizes)
       }
     });
-    
-    // -----------------------------------------------------------------
+
     // Redefine CSS variables for each theme
-    // -----------------------------------------------------------------
 
     themes.forEach((theme) => {
       const { colors, name } = theme;
@@ -186,9 +189,7 @@ module.exports = plugin(
       });
     });
 
-    // -----------------------------------------------------------------
-    // BONUS: Add theme-specific variant for bespoke theming overrides
-    // -----------------------------------------------------------------
+    //  Add theme-specific variant for bespoke theming overrides
 
     themes.forEach((theme) => {
       addVariant(`theme-${theme.name}`, `[data-theme=${theme.name}] &`);
@@ -198,32 +199,9 @@ module.exports = plugin(
       addVariant(`size-${size.name}`, `[data-size=${size.name}] &`);
     });
   },
-
-  // -----------------------------------------------------------------
-  // Add semantic color names to Tailwind's color palette
-  // -----------------------------------------------------------------
-
   {
     theme: {
       extend: {
-        colors: {
-          // All the colors for good measure, for unique use-cases.
-          ...themes.reduce((acc, theme) => {
-            return {
-              ...acc,
-              [`${theme.name}-base`]: theme.colors["bg-base"],
-              [`${theme.name}-active`]: theme.colors["bg-active"],
-              [`${theme.name}-inverted`]: theme.colors["bg-inverted"],
-              [`${theme.name}-pure`]: theme.colors["bg-pure"],
-              [`${theme.name}-text-base`]: theme.colors["text-base"],
-              [`${theme.name}-text-active`]: theme.colors["text-active"],
-              [`${theme.name}-text-inverted`]: theme.colors["text-inverted"],
-              [`${theme.name}-border-base`]: theme.colors["border-base"],
-              [`${theme.name}-border-active`]: theme.colors["border-active"],
-              [`${theme.name}-border-inverted`]: theme.colors["border-inverted"]
-            };
-          }, {})
-        },
         textColor: {
           theme: {
             base: "rgb(var(--color-text-base) / <alpha-value>)",
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
+    universalUI: {
+      themes: [
+        {
+          name: "neutral",
+          color: "orange"
+        },
+        {
+          name: "error",
+          color: "pink"
+        }
+      ]
+    }
   },
-  plugins: [
-     require('./src/config/plugin.cjs'),
-  ]
+  plugins: [require("./src/config/plugin.cjs")]
 };
